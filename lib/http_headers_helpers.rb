27b0a914905735bef99414fde1f560479122e298
Thomas Cannon <tcannon@codemonkeydev.com>
2013-07-17 17:17:07 -0400

    Added helpers for HTTP statuses and headers
    Since we want to include the expected HTTP Headers for each response, I added basic helpers for rendering HTTP status and headers (code yanked from: https://github.com/github/developer.github.com/blob/master/lib/resources.rb#L10)

---
+++ lib/http_headers_helpers.rb
@@ -0,0 +1,41 @@
+module Freckle
+	module HTTPHeaders
+		module Helpers
+			STATUSES = {
+        200 => '200 OK',
+        201 => '201 Created',
+        202 => '202 Accepted',
+        204 => '204 No Content',
+        205 => '205 Reset Content',
+        301 => '301 Moved Permanently',
+        302 => '302 Found',
+        307 => '307 Temporary Redirect',
+        304 => '304 Not Modified',
+        401 => '401 Unauthorized',
+        403 => '403 Forbidden',
+        404 => '404 Not Found',
+        405 => '405 Method not allowed',
+        409 => '409 Conflict',
+        422 => '422 Unprocessable Entity',
+        500 => '500 Server Error'
+      }
+
+      def headers(status, head = {})
+        css_class = (status == 204 || status == 404) ? 'headers no-response' : 'headers'
+        lines = ["Status: #{STATUSES[status]}"]
+        head.each do |key, value|
+          case key
+            when :pagination
+              lines << 'Link: <https://api.github.com/resource?page=2>; rel="next",'
+              lines << ' <https://api.github.com/resource?page=5>; rel="last"'
+            else lines << "#{key}: #{value}"
+          end
+        end
+
+        %(<pre class="#{css_class}"><code>#{lines * "\n"}</code></pre>\n)
+      end
+		end
+	end
+end
+
+include Freckle::HTTPHeaders::Helpers
