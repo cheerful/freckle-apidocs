325253432b7f708da8c0cbc5a543482804c6f1ee
Thomas Cannon <tcannon@codemonkeydev.com>
2013-07-17 13:13:50 -0400

    Wrote framework for rendering consistent API objects in JSON
    Inspired by the [helpers used in the Github API](https://github.com/github/developer.github.com/blob/master/lib/resources.rb), I have written a custom tag that will render a pre-defined API object (stored as a ruby hash in our _plugins directory) as JSON. These objects can reference each other, allowing us to build consistent API objects in our documentation with minimal effort.

---
+++ _plugins/api_objects.rb
@@ -0,0 +1,43 @@
+module Jekyll
+	module APIObjects
+		USER_SHORT = {
+			"id" =>  5538,
+    	"email" =>  "john.test@test.com",
+    	"first_name" =>  "John",
+    	"last_name" =>  "Test",
+    	"avatar" => {
+    		"id" =>  5538,
+    		"thumbnail" =>  "http=> //apitest.letsfreckle.com/images/avatars/0000/0001/avatar_profile.jpg",
+				"avatar" =>  "http=> //apitest.letsfreckle.com/images/avatars/0000/0001/avatar.jpg"
+    	},
+    	"url" =>  "http=> //apitest.letsfreckle.com/api/users/5538",
+		}
+
+
+		ACCOUNT = {
+			"account" => {
+				"id" =>  3344,
+				"plan_name" =>  "Freelancer",
+				"subdomain" =>  "testit",
+				"title" =>  "Test It!",
+				"address" =>  {
+					"address1" =>  "1345 Main Street",
+					"address2" =>  "Room 3",
+					"city" =>  "Mainsville",
+					"state" =>  "MA",
+					"zip" =>  "12345",
+					"country" =>  "USA",
+					"phone" =>  "(111)-222-3333",
+				},
+
+				"ccinvoices"=>  "john.test@test.com",
+				"invoicing_enabled"=>  true,
+				"owner" =>  USER_SHORT,
+
+				"url" =>  "http=> //apitest.letsfreckle.com/api/account/",
+				"created_at" =>  "2010-06-09T20=> 44=> 57Z",
+		  	"updated_at" =>  "2010-06-09T20=> 44=> 57Z",
+			}
+		}
+	end
+end
