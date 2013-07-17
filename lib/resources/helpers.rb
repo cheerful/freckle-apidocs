cb7d4e44c1c5774600ed1f45bbb413cb9406670b
Thomas Cannon <tcannon@codemonkeydev.com>
2013-07-17 17:29:08 -0400

    Separated helpers and API Resources into separate files.
    To help keep things managable, I have separated the helper methods from the API resource hashes.

---
+++ lib/resources/helpers.rb
@@ -0,0 +1,26 @@
+require 'json'
+
+module Freckle
+		module Resources
+			module Helpers
+			def json(key)
+			  hash = case key
+			    when Hash
+			      h = {}
+			      key.each { |k, v| h[k.to_s] = v }
+			      h
+			    when Array
+			      key
+			    else Resources.const_get(key.to_s.upcase)
+			  end
+
+			  hash = yield hash if block_given?
+
+			  %(<pre class="highlight"><code class="language-javascript">) +
+			    JSON.pretty_generate(hash) + "</code></pre>"
+			end
+	  end
+	end
+end
+
+include Freckle::Resources::Helpers
